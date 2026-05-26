3076e671230f16eeed52dad3adc538b1c7fc2b6d
Alexey Sedoykin <sedoykin@gmail.com>
2026-05-26 21:34:16 +0300

    Initial Commit

---
+++ QRToGo/QRToGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRToGoApp.swift
+//  QRToGo
+//
+//  Created by Sedoykin Alexey on 26/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct QRToGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
